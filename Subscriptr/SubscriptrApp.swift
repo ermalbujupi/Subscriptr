3d7006020c32ad14ebf60b092b25d9d16e97b274
Ermal Bujupaj <ermal.bj@gmail.com>
2026-05-04 21:25:59 +0200

    Initial commit

---
+++ Subscriptr/SubscriptrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptrApp.swift
+//  Subscriptr
+//
+//  Created by Ermal Bujupaj on 4.5.26.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
